df67b8bd0ec2b857fc2e8abb64e8f9ae420d80f2
MatthewRiding <116739540+MatthewRiding@users.noreply.github.com>
2025-09-17 15:32:28 +0100

    Separated code for delay matrix extraction into a function in its own file, called in the click response method of MainWindow.

--- classdefs/modmainwindow.py
+++ classdefs/modmainwindow.py
@@ -17,6 +17,7 @@ from classdefs.modisochronmanager import IsochronManager
 from functions.moddetrendfmc3d import detrend_fmc_3d
 from functions.modextractpcm import extract_pcm
 from functions.modcalculateanglematrix import calculate_pixel_ray_angle_matrix_deg
+from functions.modgeneratedelaymatrix import generate_delay_matrix
 from corevariables.modfiletypeloading import dict_loading_functions
 
 
@@ -646,59 +647,8 @@ class ChorusMainWindow(QMainWindow, Ui_MainWindow):
                     x_pixel_m, z_pixel_m = xz_coords_m_tuple
                     self.pixel_selected = True
 
-                    # Get the send and receive leg travel time calculation functions:
-                    wave_type_send = self.selected_tfm_image.tfm_constructor.wave_type_send
-                    wave_type_receive = self.selected_tfm_image.tfm_constructor.wave_type_receive
-                    calculate_delay_times_single_pixel_all_el_send_s = wave_type_send.func_calculate_delay_times_single_pixel_all_el_s
-                    calculate_delay_times_single_pixel_all_el_receive_s = wave_type_receive.func_calculate_delay_times_single_pixel_all_el_s
-
-                    # Calculate the send delays for all elements:
-                    send_delays_all_el_s = calculate_delay_times_single_pixel_all_el_send_s(x_pixel_m, z_pixel_m,
-                                                                                            self.selected_tfm_image.tfm_constructor)
-                    # Calculate the receive delays for all elements:
-                    receive_delays_all_el_s = calculate_delay_times_single_pixel_all_el_receive_s(x_pixel_m, z_pixel_m,
-                                                                                                  self.selected_tfm_image.tfm_constructor)
-
-                    # Use np.meshgrid to repeat the send and receive delay vectors into arrays:
-                    send_delays_matrix_s, receive_delays_matrix_s = np.meshgrid(send_delays_all_el_s, receive_delays_all_el_s)
-
-                    # Sum the send and receive delay matrices to obtain an n_elements by n_elements matrix of delays:
-                    self.delay_matrix_s = send_delays_matrix_s + receive_delays_matrix_s
-
-                    # Mask delay law based on gen and det mask_specs:
-                    if self.selected_tfm_image.tfm_constructor.mask_spec_gen or self.selected_tfm_image.tfm_constructor.mask_spec_det:
-                        # Get gen mask:
-                        if self.selected_tfm_image.tfm_constructor.mask_spec_gen:
-                            mask_spec_gen = self.selected_tfm_image.tfm_constructor.mask_spec_gen
-                            # Apply mask to the delay matrix based on send ray angle:
-                            gen_angle_matrix_deg = calculate_pixel_ray_angle_matrix_deg(x_pixel_m, z_pixel_m,
-                                                                                        self.selected_tfm_image.x_gen_matrix_m)
-                            # Use the logic of the chosen mask behaviour:
-                            numpy_masking_function_gen = mask_spec_gen.mask_behaviour.numpy_masking_function
-                            gen_angle_matrix_masked = numpy_masking_function_gen(gen_angle_matrix_deg,
-                                                                                 mask_spec_gen.mask_angle_deg)
-                            mask_matrix_gen = np.ma.getmask(gen_angle_matrix_masked)
-                        else:
-                            mask_matrix_gen = np.ma.nomask
-
-                        # Get det mask:
-                        if self.selected_tfm_image.tfm_constructor.mask_spec_det:
-                            mask_spec_det = self.selected_tfm_image.tfm_constructor.mask_spec_det
-                            # Apply mask to the delay matrix based on send ray angle:
-                            det_angle_matrix_deg = calculate_pixel_ray_angle_matrix_deg(x_pixel_m, z_pixel_m,
-                                                                                        self.selected_tfm_image.x_det_matrix_m)
-                            # Use the logic of the chosen mask behaviour:
-                            numpy_masking_function_det = mask_spec_det.mask_behaviour.numpy_masking_function
-                            det_angle_matrix_masked = numpy_masking_function_det(det_angle_matrix_deg,
-                                                                                 mask_spec_det.mask_angle_deg)
-                            mask_matrix_det = np.ma.getmask(det_angle_matrix_masked)
-                        else:
-                            mask_matrix_det = np.ma.nomask
-
-                        # Combine gen and det masks:
-                        mask_gen_and_det = np.ma.mask_or(mask_matrix_gen, mask_matrix_det)
-                        # Apply combined mask to delay matrix:
-                        self.delay_matrix_s = np.ma.masked_where(mask_gen_and_det, self.delay_matrix_s, copy=False)
+                    # Generate the delay matrix for this pixel and TFM image:
+                    self.delay_matrix_s = generate_delay_matrix(x_pixel_m, z_pixel_m, self.selected_tfm_image)
 
                     # Display the delays on the B-scan widgets:
                     self.b_scan_view_widget_iso_det.update_delays(self.delay_matrix_s[self.det_index, :])
