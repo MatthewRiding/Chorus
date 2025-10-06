b437b7034ec171679adc68cfec1282e6003e90d2
MatthewRiding <116739540+MatthewRiding@users.noreply.github.com>
2025-10-06 16:16:30 +0100

    Began editing file import functions and decided that numpy array format should be rotated.  Opened new branch to begin this application-wide change.

--- functions/modloadfmclpfromtxtfile.py
+++ functions/modloadfmclpfromtxtfile.py
@@ -1,16 +1,22 @@
 import numpy as np
 
-from functions.modconvert2dto3dfmcformat import convert_2d_to_3d_fmclp_format
+from functions.modconvert2dto3dfmcformat import convert_full_matrix_numpy_2d_to_3d
 
 
 def load_fmclp_from_txt_file(file_path):
-    # Load the contents of the .txt file as a 2D NumPy array:
-    # Default delimiter (whitespace) and newline (\n) characters, matching the default np.savetxt() function.
-    displacements_fmc_2d_v = np.loadtxt(file_path)
+    """
+    Load a full matrix of displacement measurements from the .txt file at file_path.
 
-    # The recorded displacement measurements are assumed to be in units of volts, output
-    # from the Sound and Bright Quartet.
+    This function is designed to be complimentary to the numpy.savetxt() function, which can save 1D and 2D Numpy
+    arrays into a delimited variable txt file.
+
+    Given that numpy.savetxt() does not support saving 3D Numpy arrays, this function expects to load a full matrix in
+    2D numpy (t, dn + g) format as a 2D numpy array, which it will then convert into 3D numpy (t, d, g) full matrix
+    format.
+    """
+    # Default delimiter (whitespace) and newline (\n) characters, matching the default np.savetxt() function.
+    displacements_fmc_2d_raw = np.loadtxt(file_path)
 
     # Convert 2D array FMC format into 3D array format:
-    displacements_fmc_3d_v = convert_2d_to_3d_fmclp_format(displacements_fmc_2d_v)
+    displacements_fmc_3d_v = convert_full_matrix_numpy_2d_to_3d(displacements_fmc_2d_raw)
     return displacements_fmc_3d_v
