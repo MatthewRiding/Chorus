f9b99183b2417d03b17b6b227d56277080870e40
MatthewRiding <116739540+MatthewRiding@users.noreply.github.com>
2025-10-09 16:16:35 +0100

    Deleted unnecessary numpy import.

--- corevariables/modfiletypeloading.py
+++ corevariables/modfiletypeloading.py
@@ -1,5 +1,3 @@
-import numpy as np
-
 from functions.modloadfullmatrixfromnpyfile import load_full_matrix_from_npy_file
 from functions.modloadfullmatrixfromtxtfile import load_full_matrix_from_txt_file
 from functions.modloadfullmatrixfrommatfile import load_full_matrix_from_mat_file
